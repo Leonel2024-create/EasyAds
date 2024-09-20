0f90c4b48cb4b16bad8a59891db3e74911faa559
Leonel2024-create <rojasleonel277@gmail.com>
2024-09-20 04:35:33 +0000

    Confirmando todos los cambios pendientes

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Bienvenido a EasyAds</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
+
+
