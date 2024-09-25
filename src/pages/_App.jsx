5b347a4efb2f14120777f5d87b7f3496cc686d3c
Leonel2024-create <rojasleonel277@gmail.com>
2024-09-25 22:57:39 +0000

    Eliminar archivo _redirects

--- src/pages/_App.jsx
+++ src/pages/_App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Campaigns from './pages/Campaigns';
-import TermsAndConditions from './components/TermsAndConditions';
-import PrivacyPolicy from './components/PrivacyPolicy';
-import Footer from './components/Footer';
+import TermsAndConditions from '../components/TermsAndConditions';
+import PrivacyPolicy from '../components/PrivacyPolicy';
+import Footer from '../components/Footer';
 
 function App() {
   return (
